1829baac0f01ed6dc9b92e7005bda15ece0f576a
Giadaspt <saputo.giada@gmail.com>
2021-12-02 17:46:57 +0100

    una sola chiamta api

--- src/App.vue
+++ src/App.vue
@@ -4,8 +4,8 @@
     @searchForMovie="callApi"
     @searchForSeries="callApi"/>
     <Main
-    :seriesDetails="responseSeries"
-    :filmDetails="responseMovies"/>
+    :seriesDetails="callSeries"
+    :filmDetails="callMovies"/>
     
   </div>
 </template>
@@ -24,55 +24,77 @@ export default {
 
   data(){
     return {
-      initURLMovie : "https://api.themoviedb.org/3/search/movie",
-      initURLSeries : "https://api.themoviedb.org/3/search/tv",
+      apiOne : "https://api.themoviedb.org/3/search/movie",
+      apiTwo : "https://api.themoviedb.org/3/search/tv",
       urlParams: {
         api_key: 'e7257db464e5496213805673eeca26f6',
         query:'',
       },
-      responseMovies: [],
-      responseSeries: [],
+      callMovies: [],
+      callSeries: [],
     }
   },
 
   methods:{
 
-    getApiMovie(){
-      
-      axios.get(this.initURLMovie, {params: this.urlParams})
-      .then(r =>{
-        //console.log('io sono r',r);
-        this.responseMovies = r.data.results;
-        console.log('response movie', this.responseMovies);
-      }).catch(e =>{
-        console.log('errore',e);
-      });
-    },
+    getApi(){
 
-    getApiSeries(){
+      let callMovie = axios.get(this.apiOne, {params: this.urlParams});
+      let callSerie = axios.get(this.apiTwo, {params: this.urlParams});
+      console.log(callMovie);
+      console.log(callSerie);
+
+      Promise.all([callMovie, callSerie])
+      .then((r) => {
+        this.callMovies = r[0].data.results;
+        this.callSeries = r[1].data.results;
+      }).catch(e => {
+        console.log(e);
+      })
+
+      // axios.all([callMovie, callSeries])
+      // .then(axios.spread(responseOne, responseTwo => {
+      //   const resp = responseOne.data.results;
+      //   const respSec = responseTwo.data.results;
+      // }))
       
-      axios.get(this.initURLSeries, {params: this.urlParams})
-      .then(r =>{
-        //console.log('io sono r',r);
-        this.responseSeries = r.data.results;
-        console.log('response series serie', this.responseSeries);
-      }).catch(e =>{
-        console.log('errore',e);
-      });
-  
     },
+    // getApiSMovie(){
+      // axios.get(this.initURLMovie, {params: this.urlParams})
+      // .then(r =>{
+      //   //console.log('io sono r',r);
+      //   this.responseMovies = r.data.results;
+      //   console.log('response movie', this.responseMovies);
+      // }).catch(e =>{
+      //   console.log('errore',e);
+      // });
+    
+
+    // getApiSeries(){
+      
+    //   axios.get(this.initURLSeries, {params: this.urlParams})
+    //   .then(r =>{
+    //     //console.log('io sono r',r);
+    //     this.responseSeries = r.data.results;
+    //     console.log('response series serie', this.responseSeries);
+    //   }).catch(e =>{
+    //     console.log('errore',e);
+    //   });
+  
+    // },
 
     callApi(text){
       this.urlParams.query = text;
-      console.log(text);
-      console.log(this.urlParams.query);
+      //console.log(text);
+      //console.log(this.urlParams.query);
 
-      this.getApiMovie();
-      this.getApiSeries();
+      this.getApi();
+      // this.getApiSeries();
 
       // console.log('movie api', this.getApiMovie());
       // console.log('series api', this.getApiSeries());
     },
+
 
   }
 
